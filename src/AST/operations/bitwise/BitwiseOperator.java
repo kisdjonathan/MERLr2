0e960e441dae4b984be0a613b9d99977f11e5966
aidan <aidan.zhlai@gmail.com>
2022-12-22 17:06:55 -0600

    bitwise op rework as well as equals impl

--- src/AST/operations/bitwise/BitwiseOperator.java
+++ src/AST/operations/bitwise/BitwiseOperator.java
@@ -2,6 +2,8 @@ package AST.operations.bitwise;
 
 import AST.abstractNode.SyntaxNode;
 import AST.baseTypes.BasicType;
+import AST.baseTypes.Bool;
+import AST.baseTypes.Char;
 import AST.operations.Operator;
 
 public abstract class BitwiseOperator extends Operator {
@@ -13,10 +15,32 @@ public abstract class BitwiseOperator extends Operator {
 
     //TODO
     public BasicType getType() {
-        return null;
+        BasicType first = getChild(0).interpret();
+        BasicType second = size() > 1 ?  getChild(1).interpret() : null;
+        if (first instanceof Bool != second instanceof Bool) {
+            throw new Error("Unsupported arguments for " + getName() + " operator: \n\tfirst: " + first.getType() + "\n\tsecond:" + (second != null ? second.getType() : null));
+        } else if (first instanceof Bool) {
+            return new Bool();
+        } else {
+            return new Char();
+        }
     }
 
     public BasicType interpret() {
-        return null;
+        BasicType first = getChild(0).interpret();
+        BasicType second = size() > 1 ?  getChild(1).interpret() : null;
+        BasicType outType = getType();
+
+        if (outType instanceof Bool) {
+            return interpretBools((Bool) first, (Bool) second);
+        } else {
+            //TODO
+            return interpretBytes((Char) first, (Char) second);
+        }
     }
+
+    protected abstract Bool interpretBools(Bool first, Bool second);
+
+    protected abstract Char interpretBytes(Char first, Char second);
+
 }
--- src/AST/operations/comparison/Ascending.java
+++ src/AST/operations/comparison/Ascending.java
@@ -1,11 +1,24 @@
 package AST.operations.comparison;
 
 import AST.abstractNode.SyntaxNode;
+import AST.baseTypes.Bool;
+import AST.baseTypes.Numerical;
 
 public class Ascending extends ComparisonOperator {
     public Ascending() {}
     public Ascending(SyntaxNode origin, SyntaxNode vector) {
         super(origin, vector);
+    }
+
+    //TODO
+    @Override
+    protected Bool interpretNumerical(Numerical first, Numerical second) {
+        return null;
+    }
+
+    @Override
+    protected Bool interpretOthers() {
+        return null;
     }
 
     public String getName() {
